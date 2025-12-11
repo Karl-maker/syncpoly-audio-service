447f486f8123c8e15765f28b16a86986f8794484
Karl-Maker <karljohanbailey98@gmail.com>
2025-12-11 01:57:53 -0400

    fix: add region

--- infra/prod/versions.tf
+++ infra/prod/versions.tf
@@ -22,3 +22,14 @@ terraform {
   }
 }
 
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "static_s3_bucket_name" {
+  description = "Name of the S3 bucket for static assets"
+  type        = string
+}
+
